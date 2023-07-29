f42dad22118a851452d328c2ac9ddd06de22f302
mukulbbhardwaj <themukulbhardwaj@gmail.com>
2023-07-29 12:23:27 +0530

    improved footer

--- src/components/misc/Footer.jsx
+++ src/components/misc/Footer.jsx
@@ -1,16 +1,35 @@
-import { Text,Box } from "@chakra-ui/react";
+import { Text, Box, Link, Image } from "@chakra-ui/react";
+import twitter from "../../asset/twitter.png";
 import React from "react";
 
 const Footer = () => {
-    return (
-        <>
-            <Box position={'fixed'} bottom={'0'} >
+  return (
+    <>
+      <Box
+        position={"relative"}
+        bottom={"0"}
+        display={"flex"}
+        color={"white"}
+        borderTop={"1px solid gray"}
+        marginTop={"75px"}
+        gap={"6px"}
+      >
+        <Text textAlign={"center"} marginTop={"10px"}>
+          made by
+        </Text>
 
-            <Text borderTop={'1px solid gray'} textAlign={'center'} width={'60%'} margin={'75px'} >
-                made by mukul
-            </Text>
-            </Box>
-        </>
-    )
-}
+        <Text textDecor={"none"} marginTop={"10px"}>
+          <Link href="https://github.com/mukulbbhardwaj" target="_blank">
+            mukul ♡
+          </Link>
+        </Text>
+      </Box>
+      <Box marginTop={'10px'} marginBottom={'75px'}>
+        <Link href="https://twitter.com/mukulbbhardwaj" target="_blank">
+          <Image src={twitter} width={"24px"}></Image>
+        </Link>
+      </Box>
+    </>
+  );
+};
 export default Footer;
